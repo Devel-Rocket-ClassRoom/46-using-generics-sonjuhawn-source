a1cbe9725142b9dbd8488affe7afd17a1af54ec9
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-12 22:39:00 +0900

    스택 완료

--- GenericStack/Program.cs
+++ GenericStack/Program.cs
@@ -1,4 +1,55 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("// 정수 스택 테스트");
+
+var intStack = new MyStack<int>(5);
+intStack.Push(10);
+intStack.Push(20);
+intStack.Push(30);
+
+Console.WriteLine("=== 정수 스택 테스트 ===");
+Console.WriteLine($"Count: {intStack.Count}");
+Console.WriteLine($"Peek: {intStack.Peek()}");
+Console.WriteLine($"Pop: {intStack.Pop()}");
+Console.WriteLine($"Pop: {intStack.Pop()}");
+Console.WriteLine($"Count: {intStack.Count}");
+
+// 문자열 스택 테스트
+var stringStack = new MyStack<string>(5);
+stringStack.Push("첫번째");
+stringStack.Push("두번째");
+stringStack.Push("세번째");
+
+Console.WriteLine("\n=== 문자열 스택 테스트 ===");
+Console.WriteLine($"Count: {stringStack.Count}");
+Console.WriteLine($"Peek: {stringStack.Peek()}");
+Console.WriteLine($"Pop: {stringStack.Pop()}");
+Console.WriteLine($"IsEmpty: {stringStack.IsEmpty}");
+
+class MyStack<T>
+{
+    T[] stack;
+    int count;
+    public int Count { get => count; } 
+    public bool IsEmpty { get => count==0; }
+    public MyStack(int capacity)
+    {
+        stack = new T[capacity];
+        count = 0;
+    }
+    public void Push(T item)
+    {
+        stack[count++] = item;
+    }
+    public T Pop()
+    {
+        return stack[--count];
+    }
+    public T Peek()
+    {
+        return stack[count - 1];
+    }
+
+}
