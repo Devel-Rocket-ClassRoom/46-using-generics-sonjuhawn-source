6445c30bc2a3e26faa6d9d7a21ed81227a07bb8c
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-12 23:33:33 +0900

    과제 완료

---
+++ GenericSwap/GenericSwap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Utils
+{
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+    public static void SwapInArray<T>(T[] array, int index1, int index2)
+    {
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    public static T FindMin<T>(T[] array) where T : IComparable
+    {
+        T min = array[0];
+        foreach (T t in array)
+        {
+            if (t.CompareTo(t) < 0)
+            {
+                min = t;
+            }
+        }
+        return min;
+    }
+    public static T FindMax<T>(T[] array) where T : IComparable
+    {
+        T max = array[0];
+        foreach (T t in array)
+        {
+            if (t.CompareTo(t) > 0)
+            {
+                max = t;
+            }
+        }
+        return max;
+    }
+    public static void Reverse<T>(T[] array)
+    {
+        Array.Reverse(array);
+    }
+}
--- GenericSwap/Program.cs
+++ GenericSwap/Program.cs
@@ -1,4 +1,40 @@
 using System;
+using System.Linq;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("// Swap 테스트");
+
+int x = 10, y = 20;
+Console.WriteLine($"교환 전: x={x}, y={y}");
+Utils.Swap(ref x, ref y);
+Console.WriteLine($"교환 후: x={x}, y={y}");
+
+string a = "Hello", b = "World";
+Console.WriteLine($"\n교환 전: a={a}, b={b}");
+Utils.Swap(ref a, ref b);
+Console.WriteLine($"교환 후: a={a}, b={b}");
+
+// SwapInArray 테스트
+int[] numbers = { 1, 2, 3, 4, 5 };
+Console.WriteLine($"\n배열 교환 전: [{string.Join(", ", numbers)}]");
+Utils.SwapInArray(numbers, 0, 4);
+Console.WriteLine($"배열 교환 후: [{string.Join(", ", numbers)}]");
+
+// FindMin, FindMax 테스트
+int[] scores = { 85, 92, 78, 95, 88 };
+Console.WriteLine($"\n점수: [{string.Join(", ", scores)}]");
+Console.WriteLine($"최소값: {Utils.FindMin(scores)}");
+Console.WriteLine($"최대값: {Utils.FindMax(scores)}");
+
+string[] words = { "banana", "apple", "cherry" };
+Console.WriteLine($"\n단어: [{string.Join(", ", words)}]");
+Console.WriteLine($"사전순 첫 단어: {Utils.FindMin(words)}");
+Console.WriteLine($"사전순 끝 단어: {Utils.FindMax(words)}");
+
+// Reverse 테스트
+int[] data = { 1, 2, 3, 4, 5 };
+Console.WriteLine($"\n뒤집기 전: [{string.Join(", ", data)}]");
+Utils.Reverse(data);
+Console.WriteLine($"뒤집기 후: [{string.Join(", ", data)}]");
+
+
