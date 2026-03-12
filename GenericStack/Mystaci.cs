81f11e4c9924ef3d1badb641591a2a64cd0e54c4
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-12 22:40:23 +0900

    클래스 분리

---
+++ GenericStack/Mystaci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class MyStack<T>
+{
+    T[] stack;
+    int count;
+    public int Count { get => count; }
+    public bool IsEmpty { get => count == 0; }
+    public MyStack(int capacity)
+    {
+        stack = new T[capacity];
+        count = 0;
+    }
+    public void Push(T item)
+    {
+        stack[count++] = item;
+    }
+    public T Pop()
+    {
+        return stack[--count];
+    }
+    public T Peek()
+    {
+        return stack[count - 1];
+    }
+
+}
--- GenericStack/Program.cs
+++ GenericStack/Program.cs
@@ -28,28 +28,3 @@ Console.WriteLine($"Peek: {stringStack.Peek()}");
 Console.WriteLine($"Pop: {stringStack.Pop()}");
 Console.WriteLine($"IsEmpty: {stringStack.IsEmpty}");
 
-class MyStack<T>
-{
-    T[] stack;
-    int count;
-    public int Count { get => count; } 
-    public bool IsEmpty { get => count==0; }
-    public MyStack(int capacity)
-    {
-        stack = new T[capacity];
-        count = 0;
-    }
-    public void Push(T item)
-    {
-        stack[count++] = item;
-    }
-    public T Pop()
-    {
-        return stack[--count];
-    }
-    public T Peek()
-    {
-        return stack[count - 1];
-    }
-
-}
